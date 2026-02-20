289c16a250071f886032ba7b073428009b895652
AdultyFlix <AdultyFlix@Gmail.Com>
2026-02-20 15:06:01 +0600

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -20,11 +20,11 @@ if not os.path.exists(SAVE_FOLDER):
 
 # ================= CHROME OPTIONS =================
 options = Options()
-options.add_argument("--headless=new")
+options.add_argument("--start-maximized")
+options.add_argument("--user-data-dir=C:/selenium-profile")
+options.add_argument("--remote-debugging-port=9222")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-options.add_argument("--window-size=1920,1080")
 
 # Auto install correct ChromeDriver
 service = Service(ChromeDriverManager().install())
